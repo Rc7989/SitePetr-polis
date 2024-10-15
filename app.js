dcb3d0a48dc27a8107daadbd00e450744377aeed
Rc <ronaldocarvalho798@gmail.com>
2024-10-15 14:41:22 -0300

    fix clima

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const API_KEY = "c81f4dafe5d2c205fda8c987771b3d5c"; // Substitua pela sua chave
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-const DEFAULT_LOCATION = "centro,petrópolis"; // Cidade padrão
+const DEFAULT_LOCATION = "cascatinha,petrópolis"; // Cidade padrão
 
 const diasDaSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
 const meses = [
@@ -75,7 +75,7 @@ async function getWeather(location) {
 
     const weatherInfoDiv = document.getElementById("top");
     weatherInfoDiv.innerHTML = `
-            <p class="temp">${temperaturaArredondada-2}°</p>
+            <p class="temp">${temperaturaArredondada-1}°</p>
             <img class="clima" src="${imageUrl}" alt="${descricaoEmPortugues}">
         `;
     const bottom = document.getElementById("bot");
